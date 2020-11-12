84aad9a01e5f024964b7ba414eef19bdacfeb05d
lissomliksom <lissomliksom@gmail.com>
2020-11-12 21:01:31 +0100

    Added basic content

---
+++ src/components/Portfolio.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return(
+    <div className="About">
+      <h2>Portfolio</h2>
+      <section className="projects">
+        <h3>Projects</h3>
+
+        <h4>Lesekloden</h4>
+        <div className="details">
+          <p>Technologies, frameworks, libraries</p>
+        </div>
+
+        <h4>Portfolio</h4>
+        <div className="details">
+          <p>Technologies, frameworks, libraries</p>
+        </div>
+
+        <h4>Greyp</h4>
+        <div className="details">
+          <p>Technologies, frameworks, libraries</p>
+        </div>
+      </section>
+      
+      <h3>Other</h3>
+    </div>
+  )
+}
+
+export default Portfolio;
