f49252bd150b60ccffd138e98c60966ea7be48a1
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2022-09-28 23:26:21 +0200

    Formattering av kode og opprydding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Header from './components/Header'
-import Bio from './components/Bio'
-import WorkMajor from './components/WorkMajor'
-import WorkMinor from './components/WorkMinor'
-import Contact from './components/Contact'
+import Header from "./components/Header";
+import Bio from "./components/Bio";
+import WorkMajor from "./components/WorkMajor";
+import WorkMinor from "./components/WorkMinor";
+import Contact from "./components/Contact";
 
 const App = () => {
-
-  const [theme, setTheme] = useState('')
+  const [theme, setTheme] = useState("");
 
   return (
     <div className={theme}>
       <div className="transition-all duration-500 ease-in-out bg-white dark:bg-zinc-800">
-        <Header 
-          theme={theme} 
-          setTheme={setTheme} 
-        />
+        <Header theme={theme} setTheme={setTheme} />
         <Bio />
         <WorkMajor />
         <WorkMinor />
@@ -24,6 +20,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       grayscale: {
-        25: '25%',
-        75: '75%',
-      }
-    }
+        25: "25%",
+        75: "75%",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
